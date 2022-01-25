96d22d284b8d4c696b45dc177da181d661e5b446
soimyy <dayanbiefu7@gmail.com>
2022-01-26 00:14:56 +0900

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "styles/Home.module.css";
-import { ProductList } from "@/components/ProductList";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from 'styles/Home.module.css'
+import { ProductList } from '@/components/ProductList'
 
 export default function Home() {
   return (
     <div className={styles.ProductList}>
       <ProductList />
     </div>
-  );
+  )
 }
